0aa7603cefa54f056d68db0074e4a3d94d0a4d9d
unknown <rherl23@gmail.com>
2022-02-15 17:50:23 -0600

    init

---
+++ WF_Numeros/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿
+namespace WF_Numeros
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.inputDecimal = new System.Windows.Forms.TextBox();
+            this.inputBinary = new System.Windows.Forms.TextBox();
+            this.inputHex = new System.Windows.Forms.TextBox();
+            this.inputOctal = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(28, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(181, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Conversion System";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Enter a Number";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 93);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Binary";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(28, 122);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(76, 15);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Hexadecimal";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(28, 152);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 15);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Octal";
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(28, 225);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(75, 23);
+            this.btnConvert.TabIndex = 6;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(142, 224);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 7;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(254, 224);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 8;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // inputDecimal
+            // 
+            this.inputDecimal.Location = new System.Drawing.Point(124, 56);
+            this.inputDecimal.Name = "inputDecimal";
+            this.inputDecimal.Size = new System.Drawing.Size(100, 23);
+            this.inputDecimal.TabIndex = 9;
+            // 
+            // inputBinary
+            // 
+            this.inputBinary.Location = new System.Drawing.Point(124, 85);
+            this.inputBinary.Name = "inputBinary";
+            this.inputBinary.ReadOnly = true;
+            this.inputBinary.Size = new System.Drawing.Size(100, 23);
+            this.inputBinary.TabIndex = 10;
+            // 
+            // inputHex
+            // 
+            this.inputHex.Location = new System.Drawing.Point(124, 114);
+            this.inputHex.Name = "inputHex";
+            this.inputHex.ReadOnly = true;
+            this.inputHex.Size = new System.Drawing.Size(100, 23);
+            this.inputHex.TabIndex = 12;
+            // 
+            // inputOctal
+            // 
+            this.inputOctal.Location = new System.Drawing.Point(124, 144);
+            this.inputOctal.Name = "inputOctal";
+            this.inputOctal.ReadOnly = true;
+            this.inputOctal.Size = new System.Drawing.Size(100, 23);
+            this.inputOctal.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(359, 280);
+            this.Controls.Add(this.inputOctal);
+            this.Controls.Add(this.inputHex);
+            this.Controls.Add(this.inputBinary);
+            this.Controls.Add(this.inputDecimal);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Conersion System v1.0.0";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.TextBox inputDecimal;
+        private System.Windows.Forms.TextBox inputBinary;
+        private System.Windows.Forms.TextBox inputHex;
+        private System.Windows.Forms.TextBox inputOctal;
+    }
+}
+
---
+++ WF_Numeros/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WF_Numeros
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Clear button clear
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            this.inputDecimal.Text = "";
+            this.inputBinary.Text = "";
+            this.inputHex.Text = "";
+            this.inputOctal.Text = "";
+        }
+
+        //Exit button click
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        //Convert button click
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            int _number = 0;
+            if (int.TryParse(this.inputDecimal.Text,out _number))
+            {
+                this.inputBinary.Text = this._toBinary(_number);
+                this.inputHex.Text = this._toHex(_number);
+                this.inputOctal.Text = this._toOct(_number);
+            }
+        }
+
+
+        //return binary number
+        private string _toBinary(int _number)
+        {
+            if (_number <= 1)
+            {
+                return _number.ToString();
+            }
+            String result = "";
+            while (_number >1)
+            {
+                int remainder = _number % 2;
+                result = remainder.ToString() + result;
+                _number /= 2;
+            }
+            return _number.ToString()+ result;
+        }
+
+        //return hex number
+        private string _toHex(int _number)
+        {
+            return _number.ToString("x").ToUpper();
+        }
+
+        //return oct number
+        private string _toOct(int _number)
+        {
+            int i = 1, j;
+            int[] octalValue = new int[80];
+            
+            while (_number != 0)
+            {
+                octalValue[i++] = _number % 8;
+                _number /= 8;
+            }
+
+            string result = "";
+            for (j = i - 1; j > 0; j--)
+            {
+                result += octalValue[j];
+            }
+            return result;
+        }
+    }
+}
